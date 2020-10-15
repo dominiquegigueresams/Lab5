51de8b6f6dafa812f63a3e4296ee5a7298b1e9a5
Shidrath-R <71675893+Shidrath-R@users.noreply.github.com>
2020-10-14 21:17:18 -0400

    Update CD.py

--- CD.py
+++ CD.py
@@ -1,6 +1,6 @@
 # SYSC3010 Lab 5 - CD.py
 #     The file to be jointly edited by Team Members C and D
-from D import* 
+from D import* # Member D(Shidrath Rahman Haider)
 def function_CD():
     print ("CD")
     Function_D()
