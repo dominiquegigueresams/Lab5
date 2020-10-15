efbdfccfc1a594907c8fd67c3a828d7664f2646c
dominiquegigueresams <71286820+dominiquegigueresams@users.noreply.github.com>
2020-10-14 21:19:25 -0400

    Update CD.py

--- CD.py
+++ CD.py
@@ -1,9 +1,11 @@
 # SYSC3010 Lab 5 - CD.py
 #     The file to be jointly edited by Team Members C and D
 from D import* # Member D(Shidrath Rahman Haider)
+from C import* # Member C (Dominique Giguere Samson)
+
 def function_CD():
     print ("CD")
-    Function_D()
-    
+    function_D()
+    funciton_C()
 
     
